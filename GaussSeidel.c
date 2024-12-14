98d1c2f74ef4fc1cbc6b54f7bb81cbde08056272
jamesru726 <jamesyu726@gmail.com>
2024-12-14 01:55:44 -0600

    Add files via upload

---
+++ GaussSeidel.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>  // For malloc
+
+void *GaussSeidel(int x, int y, double *residual, double *A_N, double *A_W, double *A_C, double *A_E, double *A_S, double *phi, double *phi_old, double *p_x, double lambda, int max_iters, double tol){
+
+    for(int iters = 0; iters < max_iters; iters++){
+        double max_residual = 0.0;  // To track the max residual
+
+        // Gauss-Seidel iteration
+        for(int i = 1; i < (y - 1); i++){
+            for(int j = 1; j < (x - 1); j++){
+                // Update phi using Gauss-Seidel formula
+                phi[i * y + j] = ((lambda / A_C[i * y + j]) * (p_x[i * y + j] 
+                    - (A_N[i * y + j] * phi[(i - 1) * y + j] 
+                    + A_W[i * y + j] * phi[(i * y) + j - 1] 
+                    + A_E[i * y + j] * phi[(i * y) + j + 1] 
+                    + A_S[i * y + j] * phi[(i + 1) * y + j]))) 
+                    + (1 - lambda) * phi_old[i * y + j]; 
+            }
+        }
+
+        // Calculate residual for each iteration
+        for(int i = 1; i < (y - 1); i++){
+            for(int j = 1; j < (x - 1); j++){
+                double diff = phi[i * y + j] - phi_old[i * y + j];
+                max_residual += diff * diff;  // Sum of squared differences
+            }
+        }
+
+        if (max_residual < tol){
+            *residual = sqrt(max_residual);
+            break;
+        }   
+    }
+
+}
